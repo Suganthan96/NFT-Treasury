2052c1d52fde0e6ecfe61ba7ed56f8063860e9d6
Suganthan96 <suganthan.27it@licet.ac.in>
2025-08-10 00:58:26 +0530

    navigation added for pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,102 @@
-import { Link } from "react-router-dom";
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import "./Navbar.css";
 
-export default function Navbar() {
+const Navbar: React.FC = () => {
   return (
-    <nav className="navbar">
+    <StyledWrapper>
       <div className="navbar-logo">NFT Hub</div>
-      <ul className="navbar-links">
-        <li><Link to="/home">Home</Link></li>
-        <li><Link to="/minter">Minter</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/profile">Profile</Link></li>
-      </ul>
-    </nav>
+      <div className="nav">
+        <div className="container">
+          <Link className="btn" to="/home">Home</Link>
+          <Link className="btn" to="/minter">Minter</Link>
+          <Link className="btn" to="/about">About</Link>
+          <Link className="btn" to="/profile">Profile</Link>
+          <svg className="outline" overflow="visible" width={400} height={60} viewBox="0 0 400 60" xmlns="http://www.w3.org/2000/svg">
+            <rect className="rect" pathLength={100} x={0} y={0} width={400} height={60} fill="transparent" strokeWidth={5} />
+          </svg>
+        </div>
+      </div>
+    </StyledWrapper>
   );
-} 
+}
+
+const StyledWrapper = styled.div`
+  .outline {
+    position: absolute;
+    inset: 0;
+    pointer-events: none;
+  }
+
+  .rect {
+    stroke-dashoffset: 5;
+    stroke-dasharray: 0 0 10 40 10 40;
+    transition: 0.5s;
+    stroke: #fff;
+  }
+
+  .nav {
+    position: relative;
+    width: 400px;
+    height: 60px;
+  }
+
+  .container:hover .outline .rect {
+    transition: 999999s;
+    stroke-dashoffset: 1;
+    stroke-dasharray: 0;
+  }
+
+  .container {
+    position: absolute;
+    inset: 0;
+    background: #bef6;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    padding: 0.5em;
+  }
+
+  .btn {
+    padding: 0.5em 1.5em;
+    color: #fff;
+    cursor: pointer;
+    transition: 0.1s;
+    text-decoration: none;
+    font-size: 1em;
+  }
+
+  .btn:hover {
+    background: #fff3;
+  }
+
+  .btn:nth-child(1):hover ~ svg .rect {
+    stroke-dashoffset: 0;
+    stroke-dasharray: 0 2 8 73.3 8 10.7;
+  }
+
+  .btn:nth-child(2):hover ~ svg .rect {
+    stroke-dashoffset: 0;
+    stroke-dasharray: 0 12.6 9.5 49.3 9.5 31.6;
+  }
+
+  .btn:nth-child(3):hover ~ svg .rect {
+    stroke-dashoffset: 0;
+    stroke-dasharray: 0 24.5 8.5 27.5 8.5 55.5;
+  }
+
+  .btn:nth-child(4):hover ~ svg .rect {
+    stroke-dashoffset: 0;
+    stroke-dasharray: 0 34.7 6.9 10.2 6.9 76;
+  }
+
+  .btn:hover ~ .outline .rect {
+    stroke-dashoffset: 0;
+    stroke-dasharray: 0 0 10 40 10 40;
+    transition: 0.5s !important;
+  }
+`;
+
+export default Navbar; 
